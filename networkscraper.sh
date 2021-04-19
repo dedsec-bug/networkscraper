08fbdfcbaa6ed76c3513dcbfdae579892da94586
dedsec-bug <69110150+dedsec-bug@users.noreply.github.com>
2021-04-19 15:12:09 +0000

    Create networkscraper.sh

---
+++ networkscraper.sh
@@ -0,0 +1,12 @@
+echo -e "\e[1;33m[+]Finding all the Live IP's\e[0m"
+ipfinder live_domains.txt ipfinder.txt
+
+echo -e "\e[1;33m[+]Finding IP's through ASN block's\e[0m"
+asnip -t $1 -p 
+cat ipfinder.txt cidrs.txt >> tmp-ip.txt
+
+echo -e "\e[1;33m[+]Finding IP's through DNS Buffer's\e[0m"
+curl -s https://dns.bufferover.run/dns?q=$1 | grep $1 | grep -Eo '[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}' | sort -u >> tmp-ip.txt
+
+echo -e "\e[1;33m[+]Cleaning IP list 's\e[0m"
+sort -u tmp-ip.txt >> cidr.txt
