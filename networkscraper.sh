9175efd7358d81e440ca8473b0ff9e7e2ecec1f6
dedsec-bug <69110150+dedsec-bug@users.noreply.github.com>
2021-04-20 21:25:15 +0530

    Update networkscraper.sh

--- networkscraper.sh
+++ networkscraper.sh
@@ -1,9 +1,9 @@
 echo -e "\e[1;33m[+]Finding IP's through ASN block's\e[0m"
 asnip -t $1 -p 
-cat ipfinder.txt cidrs.txt >> tmp-ip.txt
+cat ipfinder.txt cidrs.txt >> ~/recon/$1/$(date +"%d-%m-%Y")/tmp-ip.txt
 
 echo -e "\e[1;33m[+]Finding IP's through DNS Buffer's\e[0m"
-curl -s https://dns.bufferover.run/dns?q=$1 | grep $1 | grep -Eo '[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}' | sort -u >> tmp-ip.txt
+curl -s https://dns.bufferover.run/dns?q=$1 | grep $1 | grep -Eo '[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}' | sort -u >> ~/recon/$1/$(date +"%d-%m-%Y")/tmp-ip.txt
 
 echo -e "\e[1;33m[+]Cleaning IP list 's\e[0m"
-sort -u tmp-ip.txt >> cidr.txt
+sort -u ~/recon/$1/$(date +"%d-%m-%Y")/tmp-ip.txt >> ~/recon/$1/$(date +"%d-%m-%Y")/cidr.txt
